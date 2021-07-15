5c245a51aaa74782b6b600ec1ed62f45dab99b53
Carrie Vo <carrienhivo@gmail.com>
2021-07-14 22:12:31 -0700

    created my lab 2 by adding about me, page details, prompts, alerts, etc.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About Me
 
-// this is where your paragraoh will go
+My name is Carrie Vo. I received my B.A. in Developmental Child Psychology at the University of Minnesota. I have some background in mortgage which opened the door for me at Zillow on the Data Acquisition Team where I acquire and review public data. I've always been fascinated with how our website and the software we use work, so I'm really excited to learn more about it and transition into a career in Software Development!
 
 ### Author: Carrie Vo
 
--- js/app.js
+++ js/app.js
@@ -1,20 +1,76 @@
 'use strict';
 
 // get the users name so we can greet them
-let username = prompt('What is your name?');
+let username = prompt('Hey there! What is your name?');
 //greet the user
-alert('Hello ' + username);
+alert('Hi ' + username + '. Lets play a guessing game!');
 
 // ask question 1
-let answerOne = prompt('Do I like Strawberries? Answer \'yes\' or \'no\'').toLowerCase();
+let answerOne = prompt('Are you ready? Answer \'yes\' or \'no\'').toLowerCase();
 
 console.log('user entered to question 1: ' + answerOne);
 
 // check if the answer is correct:
 if (answerOne === 'yes' || answerOne === 'y') {
-  alert('You are correct. I like strawberries.');
+  alert('Perfect, let\'s start!');
 } else if (answerOne === 'no' || answerOne === 'n') {
-  alert('Sorry, you are wrong.');
+  alert('Aw come on now, you got this!');
+} else {
+  alert('Sorry, answers need to be yes or no.')
+}
+
+// ask question 2
+let answerTwo = prompt('Can you guess what state I used to live in?').toLowerCase();
+
+console.log('user entered to question 2: ' + answerTwo);
+
+// check if the answer is correct:
+if (answerTwo === 'yes' || answerTwo === 'y') {
+  alert('Oh wow, I\'m impressed. You\'re right, it\'s Minnesota!');
+} else if (answerTwo === 'no' || answerTwo === 'n') {
+  alert('It\'s okay, I\'m from Minnesota! Listen for my \'O\'s\' & \'A\'s.');
+} else {
+  alert('Sorry, answers need to be yes or no.')
+}
+
+// ask question 3
+let answerThree = prompt('Can you guess where I live now?').toLowerCase();
+
+console.log('user entered to question 3: ' + answerThree);
+
+// check if the answer is correct:
+if (answerThree === 'yes' || answerThree === 'y') {
+  alert('Oh wow, you really know a lot about me. You\'re right, it\'s California!');
+} else if (answerThree === 'no' || answerThree === 'n') {
+  alert('I live in California! The weather is a lot nicer than Minne-SNOW-ta\'s.');
+} else {
+  alert('Sorry, answers need to be yes or no.')
+}
+
+// ask question 4
+let answerFour = prompt('Take a guess, am I a cat person?').toLowerCase();
+
+console.log('user entered to question 4: ' + answerFour);
+
+// check if the answer is correct:
+if (answerFour === 'yes' || answerFour === 'y') {
+  alert('Sorry, wrong answer. Cats are cute but they kind of scare me. I\'m a diehard dog person!');
+} else if (answerFour === 'no' || answerFour === 'n') {
+  alert('You\'re right! I love dogs and have 2 very cute ones!');
+} else {
+  alert('Sorry, answers need to be yes or no.')
+}
+
+// ask question 5
+let answerFive = prompt('Lastly, are you enjoying this guessing game?').toLowerCase();
+
+console.log('user entered to question 5: ' + answerFive);
+
+// check if the answer is correct:
+if (answerFive === 'yes' || answerFive === 'y') {
+  alert('Yay! Me too, thanks for playing! You may now enter my site..');
+} else if (answerFive === 'no' || answerFive === 'n') {
+  alert('Rude. However, I\'ll be nice and let you enter my site. Go on now..');
 } else {
   alert('Sorry, answers need to be yes or no.')
 }
