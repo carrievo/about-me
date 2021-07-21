aa4342df070008137aa24c044831433e8a1bc62b
Carrie Vo <carrienhivo@gmail.com>
2021-07-20 19:36:05 -0700

    made updates and changed to about-me assignment

--- js/app.js
+++ js/app.js
@@ -2,52 +2,68 @@
 
 // get the users name so we can greet them
 let username = prompt('Hey there! What is your name?');
+
+//create score.
+let correctAnswerCount = 0;
+
 //greet the user
 alert('Hi ' + username + '. Lets play a guessing game!');
 
 // ask question 1
 let answerOne = prompt('Are you ready? Answer \'yes\' or \'no\'').toLowerCase();
 
-
 console.log('user entered to question 1: ' + answerOne);
 
 // check if the answer is correct:
+// if the answer is correct
 if (answerOne === 'yes' || answerOne === 'y') {
   alert('Perfect, let\'s start!');
+    // adds 20 to score
+    correctAnswerCount++;
+  // if answer is incorrect
 } else if (answerOne === 'no' || answerOne === 'n') {
   alert('Aw come on now, you got this!');
+  // if user answers with anything other than y/n
 } else {
-  alert('Sorry, answers need to be yes or no.')
+  alert('Sorry, answers need to be yes or no.');
 }
 
 // ask question 2
 let answerTwo = prompt('Can you guess what state I used to live in?').toLowerCase();
-l
 
 console.log('user entered to question 2: ' + answerTwo);
 
 // check if the answer is correct:
+// if answer is correct
 if (answerTwo === 'yes' || answerTwo === 'y') {
   alert('Oh wow, I\'m impressed. You\'re right, it\'s Minnesota!');
+    // adds 20 to score
+    correctAnswerCount++;
+  // if answer is incorrect
 } else if (answerTwo === 'no' || answerTwo === 'n') {
   alert('It\'s okay, I\'m from Minnesota! Listen for my \'O\'s\' & \'A\'s.');
+  //if user answers anything other than y/n
 } else {
-  alert('Sorry, answers need to be yes or no.')
+  alert('Sorry, answers need to be yes or no.');
 }
 
 // ask question 3
 let answerThree = prompt('Can you guess where I live now?').toLowerCase();
-let userScore = 0;
 
 console.log('user entered to question 3: ' + answerThree);
 
 // check if the answer is correct:
+// if answer is correct
 if (answerThree === 'yes' || answerThree === 'y') {
   alert('Oh wow, you really know a lot about me. You\'re right, it\'s California!');
+  // adds 20 to score
+  correctAnswerCount++;
+  // if answer is incorrect
 } else if (answerThree === 'no' || answerThree === 'n') {
   alert('I live in California! The weather is a lot nicer than Minne-SNOW-ta\'s.');
+  // if user answers anything other than y/n
 } else {
-  alert('Sorry, answers need to be yes or no.')
+  alert('Sorry, answers need to be yes or no.');
 }
 
 // ask question 4
@@ -56,33 +72,42 @@ let answerFour = prompt('Take a guess, am I a cat person?').toLowerCase();
 console.log('user entered to question 4: ' + answerFour);
 
 // check if the answer is correct:
+// if answer is incorrect
 if (answerFour === 'yes' || answerFour === 'y') {
-  alert('Sorry, wrong answer. Cats are cute but they kind of scare me. I\'m a diehard dog person!');
+  alert('Sorry, wrong answer. Cats are adorable but they kind of scare me. I\'m a huge dog person!');
+  // if answer is correct
 } else if (answerFour === 'no' || answerFour === 'n') {
   alert('You\'re right! I love dogs and have 2 very cute ones!');
+    // adds 20 to score
+    correctAnswerCount++;
+  // if user answers anything other than y/n
 } else {
-  alert('Sorry, answers need to be yes or no.')
+  alert('Sorry, answers need to be yes or no.');
 }
 
 // ask question 5
 let answerFive = prompt('Lastly, are you enjoying this guessing game?').toLowerCase();
-let userScore = 0;
 
 console.log('user entered to question 5: ' + answerFive);
 
 // check if the answer is correct:
+// if answer is correct
 if (answerFive === 'yes' || answerFive === 'y') {
   alert('Yay! Me too, let\'s continue!');
+    // adds 20 to score
+    correctAnswerCount++;
+  // if answer is incorrect
 } else if (answerFive === 'no' || answerFive === 'n') {
   alert('Maybe if we keep playing, it\'ll change your mind.');
+  // if user answers anything other than y/n
 } else {
-  alert('Sorry, answers need to be yes or no.')
+  alert('Sorry, answers need to be yes or no.');
 }
 
 // ask question 6
 
 let myDogNumber = 5;
-let userResponse = +prompt('Can you guess how many dogs I\'ve had in my lifetime? HINT: It is less than 8. You have 4 tries to guess, go!');
+let userResponse = +prompt('Can you guess how many dogs I\'ve had in my lifetime? \nHINT: It is less than 8. You have 4 tries to guess. \nReady? Set. Go!');
 
 let attemptsRemaining = 3;
 let attempts = 3;
@@ -94,12 +119,14 @@ while (attemptsRemaining) {
     if (userResponse === myDogNumber){
       attemptsRemaining = 0;
       alert(`Woo hoo! You're right!`);
+      correctAnswerCount++;
       break;
     } 
     else if (userResponse < myDogNumber){
-      
-      alert(`Too low, guess again! You have ${attemptsRemaining} attempts remaining.`);
+
       userResponse = +prompt(`Try another number.`);
+      alert(`Too low, guess again! You have ${attemptsRemaining} attempts remaining.`);
+      
     }  
     else if (userResponse > myDogNumber){
       
@@ -119,34 +146,55 @@ while (attemptsRemaining) {
 
 // ask question 7
 
-let answerCountries = 17;
-let attempts = 6;
+// arrays of correct answers
+let listOfAnswers = ['purple', 'yellow', 'gray', 'gold']
 
-for (let i = 1; i < attempts; i + 1) {
-    let answerAmount = +prompt ('Can you guess how many countries I\'ve been to? Hint: It\'s between 10 to 20'); {
-        console.log (`Guess of countries: ${answerCountries}`)
+// sets attempts to 6 for next question
+attempts = 6
 
-  } if (answerAmount === answerCountries) {
-      alert ('Yay, you got it!');
+while (attempts) {
+  // prompts question and gives possible answers
+  let answer = prompt(`What is one of my favorite colors? \nHint: Remember, I'm from Minnesota ;) \nYou've got 6 guesses. Good luck!`);
+
+  // logs answer to console
+  console.log(`answer = ${answer}`);
+
+  // subtracts 1 from attempts
+  attempts--;
+
+  let correct = false;
+  
+  // loops through correct answers to see if answer is correct
+  for (let i = 0; i < listOfAnswers.length; i++) {
+    if (answer.toLowerCase() === listOfAnswers[i]) {
+      alert(`You got it, ${listOfAnswers[i]} is the best color!`);
+      // sets correct to true to break while loop
+      correct = true;
       break;
-
-  } else if (answerAmount < answerCountries) {
-    attempts--;
-      alert (`You are too low! You have have ${attempts} guesses left`);
-      
-  } else if (answerAmount > answerCountries){
-    attempts--;  
-    alert (`You are too high! You have ${attempts} guesses left`);
-    
-  }
-    attemptsRemaining--;
-    if (attemptsRemaining === 0){
-      alert('The correct answer is ' + 17);
-      }
+    } else {
+      continue;
     }
-
+  }
+  if (correct == true){
+    // increments correctAnswerCount by 1
+    correctAnswerCount++;
+    // sets attempts to 0 to break while loop
+    break;
+  } else {
+    alert(`That\'s incorrect. You have ${attempts} attempts left.`);
+  }
+}
+  
 // Tell the user how many answers they got correct
 
+// calculate score
 
+let score = correctAnswerCount / 7
+score = Math.round(score * 100)
+
+// evauates and then alerts the score to the user
+if (correctAnswerCount) {
+  alert(`Thanks for playing ${username}! \n You scored ${score}%. \n You may now enter my site.`)
+}
 
 
